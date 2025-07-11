38a0546ab94259e904726068d90091aac84039ec
Anthony Zheng <anthonyzheng124@gmail.com>
2025-07-11 18:32:38 -0400

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "../server/db";
 
 const mockUrls = [
   "https://5jg4mxr6uu.ufs.sh/f/069k1fb8afrPTTPMHxR3jmXVy8Up5rWza0Rs6GA4wxEHSPLg",
@@ -11,10 +12,15 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
