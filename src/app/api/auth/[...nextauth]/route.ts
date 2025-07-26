f426d98f6e9e765b2db70425cc83ae88d02f433b
Anthony Zheng <anthonyzheng124@gmail.com>
2025-07-26 02:48:41 -0400

    cast the NextAuth function to prevent <any> type error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,19 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/lib/auth";
+import type { NextRequest } from "next/server";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth(authOptions);
+const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions) as (
+  req: NextRequest,
+) => Response | Promise<Response>;
+
 export { handler as GET, handler as POST };
