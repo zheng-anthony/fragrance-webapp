6507862b592d967c611e63fa97ef1e2880b8bf2a
Anthony Zheng <anthonyzheng124@gmail.com>
2025-08-09 23:15:31 -0400

    implemented 'params' prop into fragrance profile to find id and search for the its respective object

--- src/app/fragrances/[id]/page.tsx
+++ src/app/fragrances/[id]/page.tsx
@@ -1,7 +1,45 @@
-export default function ColognePage({
-  fragrance,
+import { db } from "@/server/db";
+import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
+
+export default async function ColognePage({
+  params,
 }: {
-  fragrance: { id: number };
+  params: { id: string };
 }) {
-  return <p>Details for cologne with id</p>;
+  const fragrance = await db.query.fragrances.findFirst({
+    where: (fragrances, { eq }) => eq(fragrances.id, Number(params.id)),
+  });
+
+  if (!fragrance) {
+    return <p>fragrance not found</p>;
+  }
+  return (
+    <>
+      <p>{fragrance.name}</p>
+      <Card
+        key={fragrance.id}
+        className="relative aspect-[2/3] w-32 cursor-pointer transition-shadow hover:shadow-md"
+      >
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={fragrance.url}
+            alt={fragrance.name}
+            fill
+            className="rounded-md object-cover"
+          />
+        </div>
+
+        <CardContent className="p-2">
+          <h3 className="truncate text-xs leading-tight font-semibold">
+            {fragrance.name}
+          </h3>
+          <p className="text-muted-foreground truncate text-[10px]">
+            {fragrance.name}
+          </p>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
