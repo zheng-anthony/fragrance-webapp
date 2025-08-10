8e21f39d1ec6af4766e430ec503c8a1ea97d578b
Anthony Zheng <anthonyzheng124@gmail.com>
2025-08-10 01:30:21 -0400

    fixed params promise error

--- src/app/fragrances/[id]/page.tsx
+++ src/app/fragrances/[id]/page.tsx
@@ -1,15 +1,17 @@
 import { db } from "@/server/db";
 import Image from "next/image";
-import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
 
 export default async function ColognePage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  const id = await params;
+  const num = Number(id.id);
+
   const fragrance = await db.query.fragrances.findFirst({
-    where: (fragrances, { eq }) => eq(fragrances.id, Number(params.id)),
+    where: (fragrances, { eq }) => eq(fragrances.id, num),
   });
 
   if (!fragrance) {
